bc0216d4649efc937b45dd1f62f747032ea9d633
Jose Juan Jaramillo Polo <josejuan.jaramillopolo@gmail.com>
2023-03-12 23:14:18 -0600

    Adding Tank.h class

--- Tank.cpp
+++ Tank.cpp
@@ -1,8 +1,11 @@
 #include "Tank.h"
 
-Tank::Tank(int sensorTriggerPin, int sensorEchoPin):
+Tank::Tank(int sensorTriggerPin, int sensorEchoPin, long maxTankLevel, long minTankLevel, long triggerAlertTankLevel):
 m_sensorTriggerPin(sensorTriggerPin),
-m_sensorEchoPin(sensorEchoPin)
+m_sensorEchoPin(sensorEchoPin),
+m_maxTankLevel(maxTankLevel),
+m_minTankLevel(minTankLevel),
+m_triggerAlertTankLevel(triggerAlertTankLevel)
 {
     Serial.println("Tank constructor");
     pinMode(m_sensorTriggerPin, OUTPUT);                          // Sets the trigPin as an OUTPUT
@@ -27,14 +30,15 @@ long Tank::getCurrentDistanceMeasure() {
 
   // Calculating the distance
   distance = duration * 0.034 / 2;  // Speed of sound wave divided by 2 (go and back)
-  ESP.wdtFeed();
+  ESP.wdtFeed(); // Somehow this method got Node MCU stuck resetting WDT so let's keep it calmed. 
 
+  m_lastDistanceMeasurement = distance; //analyzeLastDistance will make a profit of this.
   return distance;
 }
 
-void Tank::printDistance(const long &distance) {
-  Serial.print("Distance: ");
-  Serial.print(distance);
+void Tank::printCurrentDistance() {
+  Serial.print("Current Distance: ");
+  Serial.print(getCurrentDistanceMeasure());
   Serial.println(" cm");
 }
 
@@ -74,7 +78,7 @@ void Tank::actionWhile(const state &currentSate) {
         Serial.println("[WIP] Tank level keeps decreasing or hasn't reached alertTankLevel");
         Serial.print("[WIP] Current level is: ");
         Serial.println(getCurrentDistanceMeasure());
-        if (getCurrentDistanceMeasure() == long(minTankLevel)) {
+        if (getCurrentDistanceMeasure() == m_minTankLevel) {
           sendChatAlert(notificationType::emptyTank);
           actionWhile(state::tankKeepsEmpty);
           return;
@@ -84,7 +88,7 @@ void Tank::actionWhile(const state &currentSate) {
       actionWhile(state::refillingDetected);
       break;
     case state::tankKeepsEmpty:
-      while (getCurrentDistanceMeasure() == minTankLevel) {
+      while (getCurrentDistanceMeasure() == m_minTankLevel) {
         Serial.println("[WIP] Tank is empty!");
         delay(250);
       }
@@ -93,9 +97,8 @@ void Tank::actionWhile(const state &currentSate) {
     case state::refillingDetected:
       Serial.println("[WIP] Refilling action detected, keep going...");
       break;
-    case state::startUsingFullTank:
-      //This state aims to prevent spam after getting the tank fulfilled
-      while (getCurrentDistanceMeasure() <= long(maxTankLevel)) {
+    case state::startUsingFullTank: //This state aims to prevent spam after getting the tank fulfilled
+      while (getCurrentDistanceMeasure() <= m_maxTankLevel) {
         Serial.println("Tank has not been used after refilling it. Nice!");
         delay(250);
       }
@@ -106,15 +109,21 @@ void Tank::actionWhile(const state &currentSate) {
   }
 }
 
-void Tank::analyzeDistance(const long &distance) {
-  if (distance == long(triggerAlertTankLevel)) {
+void Tank::analyzeLastDistance() {
+  if (m_lastDistanceMeasurement == m_triggerAlertTankLevel) {
     sendChatAlert(notificationType::alertTankLevel);
     actionWhile(state::tankLevelIsTheSame);
-  } else if (distance == long(maxTankLevel)) {
+  } else if (m_lastDistanceMeasurement == m_maxTankLevel) {
     sendChatAlert(notificationType::fullTank);
     actionWhile(state::startUsingFullTank);
-  } else if (distance == long(minTankLevel)) {
+  } else if (m_lastDistanceMeasurement == m_minTankLevel) {
     sendChatAlert(notificationType::emptyTank);
     actionWhile(state::tankKeepsEmpty);
   }
+}
+
+void Tank::smartJobRoutine()
+{
+    printCurrentDistance();
+    analyzeLastDistance();
 }
--- Tank.h
+++ Tank.h
@@ -4,10 +4,7 @@
 
 class Tank
 {
-    #define maxTankLevel 20
-    #define minTankLevel 100
-    #define triggerAlertTankLevel 30
-
+private:
     enum class notificationType { emptyTank,
                                 alertTankLevel,
                                 fullTank };
@@ -19,16 +16,18 @@ class Tank
 
     int m_sensorTriggerPin{};
     int m_sensorEchoPin{};
-
-    Tank(int sensorTriggerPin, int sensorEchoPin);
-
-    long getCurrentDistanceMeasure();
-
-    void printDistance(const long &distance);
+    long m_maxTankLevel {};
+    long m_minTankLevel {};
+    long m_triggerAlertTankLevel {};
+    long m_lastDistanceMeasurement{};
 
     void sendChatAlert(const notificationType &currentNotification);
-
     void actionWhile(const state &currentSate);
-
-    void analyzeDistance(const long &distance);
+    void analyzeLastDistance();
+    
+public:
+    Tank(int sensorTriggerPin, int sensorEchoPin, long maxTankLevel, long minTankLevel, long triggerAlertTankLevel);
+    void smartJobRoutine();
+    long getCurrentDistanceMeasure();
+    void printCurrentDistance();
 };
